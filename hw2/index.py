a925da619c7aa75a1695d8bd2d4a8737272d9a1b
Robin Su <robisu@pdx.edu>
2021-10-22 03:59:53 +0000

    delete merge detritus

--- hw2/index.py
+++ hw2/index.py
@@ -8,9 +8,3 @@ class Index(MethodView):
         Render the landing page for the site.
         """
         return render_template('index.html')
-<<<<<<< HEAD
-        # model = review_model.get_model()
-        # entries = [dict(name=row[0], email=row[1], signed_on=row[2], message=row[3] ) for row in model.select()]
-        # return render_template('index.html',entries=entries)
-=======
->>>>>>> update
