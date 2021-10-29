d822306d5f8799b6b5812e537ac20d7df6a89037
Robin Su <robisu@pdx.edu>
2021-10-29 09:30:54 -0700

    final Dockerfiles for hw3

---
+++ hw3/hw2/review_model/Model.py
@@ -0,0 +1,49 @@
+class Model():
+    def select(self):
+        """
+        Gets all entries from the database
+        :return: Tuple containing all rows of database
+        """
+        pass
+
+    def select_one(self, id):
+        """
+        Retrieve a single database entry, based on rowid
+        :param id: Integer
+        """
+        pass
+
+    def insert(self, name, number, dept, rating, quarter, year, instructor, review):
+        """
+        Inserts entry into database
+        :param name: String
+        :param number: String
+        :param dept: String
+        :param rating: Integer
+        :param quarter: String
+        :param year: Integer
+        :param instructor: String
+        :param review: String
+        :return: none
+        :raises: Database errors on connection and insertion
+        """
+        pass
+
+    def update(self, id, name, number, dept, rating, quarter, year, instructor, review):
+        """
+        Updates existing entry in database, based on entry id.
+        :param id: Integer
+        :param name: String
+        :param number: String
+        :param dept: String
+        :param rating: Integer
+        :param quarter: String
+        :param year: Integer
+        :param instructor: String
+        :param review: String
+        :return: none
+        :raises: Database errors on connection and insertion
+        """
+        pass
+
+    
